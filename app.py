b5aa1615b602fad0c700c8dd82587ba4819e590e
Venkatesulu-123 <109635751+Venkatesulu-123@users.noreply.github.com>
2023-01-08 11:52:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,session,url_for,flash
 import sqlite3
 app = Flask(__name__)
+app.secret_key="abc"
 
 # --------------------------------indexpage---------------------------
 @app.route('/')
@@ -22,14 +23,20 @@ def register():
         password=request.form['password']
         confirmpassword=request.form['confirmpassword']
         data=[name,username,email,password,confirmpassword]
-        #print(name,username,email,password,confirmpassword)
-        if(password == confirmpassword):
-                    query="INSERT INTO registerdata(name,username,email,password,confirmpassword) VALUES (?,?,?,?,?)"
-                    cursor.execute(query,data)
-                    connection.commit()
-                    return redirect('/login')
+ #login authentications
+
+        query1="SELECT username FROM registerdata WHERE username='"+username+"'"
+        cursor.execute(query1)
+        results = cursor.fetchall()
+        if len(results) != 0:
+            return "<html><body><h1>User already exists</h1></body></html>"
+          
         else:
-            return "password not matched"
+       
+                query="INSERT INTO registerdata(name,username,email,password,confirmpassword) VALUES (?,?,?,?,?)"
+                cursor.execute(query,data)
+                connection.commit()
+                return redirect('/login')    
     return render_template('register.html')
 
 # --------------------------------loginpage---------------------------
@@ -56,12 +63,26 @@ def login():
         if len(results) == 0:
             return "userid and password is incorrect"
         else:
-            return redirect("/home")
-    return render_template('login.html')
+             session['user'] = username
+             return redirect(url_for("home"))
+    else:
+        if "user" in session:
+            return redirect(url_for("home"))
+        return render_template('login.html')
 
 # --------------------------------homepage---------------------------
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    if 'user' in session:
+        user = session['user']
+        return render_template('home.html')
+    else:
+      return redirect(url_for("login"))
+#---------logout-------
+@app.route('/logout')
+def logout():
+    session.pop("user",None)
+    return redirect(url_for("index"))
+
 if __name__ == '__main__':
     app.run(debug=True)
